bd41a92879f13473f664060038ca288b4f09cba1
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-09-11 11:09:32 +0200

    create src/main/java/com/perso/parking/controllers/

---
+++ src/main/java/com/perso/parking/controllers/ParkingController.java
@@ -0,0 +1,23 @@
+package com.perso.parking.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.perso.parking.models.Parking;
+import com.perso.parking.services.ParkingService;
+
+@RestController
+public class ParkingController {
+	
+	@Autowired
+	private ParkingService parkingService;
+	
+	@RequestMapping(path = "/api/parkings", method = RequestMethod.GET)
+	public List<Parking> getListeParkings(){	
+		return parkingService.getListeParkings();
+	}
+}
