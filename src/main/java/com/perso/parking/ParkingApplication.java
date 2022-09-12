201f6cf7e01c10a9876190fa57575cff5985f10a
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-09-12 08:33:28 +0200

    update src/main/java/com/perso/parking/ParkingApplication.java

--- src/main/java/com/perso/parking/ParkingApplication.java
+++ src/main/java/com/perso/parking/ParkingApplication.java
@@ -2,7 +2,11 @@ package com.perso.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+//Fichier de lancement et de configuration
 @SpringBootApplication
 public class ParkingApplication {
 
@@ -10,4 +14,11 @@ public class ParkingApplication {
 		SpringApplication.run(ParkingApplication.class, args);
 	}
 
+	@Bean //Bean de configuration
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+	
+		return builder.build();
+
+	}
+	
 }
