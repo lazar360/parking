b39d04b5325a2e0485a67cf38cad0d6d7471777d
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-09-21 21:55:31 +0200

    update ParkingController.java (allow cross origin)

--- src/main/java/com/perso/parking/controllers/ParkingController.java
+++ src/main/java/com/perso/parking/controllers/ParkingController.java
@@ -3,6 +3,7 @@ package com.perso.parking.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,9 @@ public class ParkingController {
 	@Autowired
 	private ParkingService parkingService;
 	
+	@CrossOrigin("http://localhost:4200")
 	@RequestMapping(path = "/api/parkings", method = RequestMethod.GET)
-	public List<Parking> getListeParkings(){	
+	public List<Parking> getListeParkings(){
 		return parkingService.getListeParkings();
 	}
 }
